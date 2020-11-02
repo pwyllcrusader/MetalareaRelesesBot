4f847a4c2bcb94943a3e5d78282e4dadae920d38
Pavel Kazak <p.kazak@godeltech.com>
2020-11-02 14:39:16 +0300

    engine improvements

--- src/main/java/by/pw/crr/metla/engine/MetalareaReleaserBot.java
+++ src/main/java/by/pw/crr/metla/engine/MetalareaReleaserBot.java
@@ -1,12 +1,72 @@
 package by.pw.crr.metla.engine;
 
+import by.pw.crr.metla.dao.ChatUserDAO;
+import by.pw.crr.metla.dao.MetalareaReleaseDAO;
+import by.pw.crr.metla.entities.ChatUser;
+import by.pw.crr.metla.entities.MetalareaRelease;
+import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class MetalareaReleaserBot extends TelegramLongPollingBot {
+
+    private final ChatUserDAO userDAO = new ChatUserDAO();
+    private final MetalareaReleaseDAO releaseDAO = new MetalareaReleaseDAO();
+
+
+    //todo more improved time task impl needed
+    public MetalareaReleaserBot() {
+        Runnable updatingTask = this::updateReleases;
+        Thread updatingThread = new Thread(updatingTask);
+        updatingThread.start();
+
+        Runnable sendingTask = this::sendReleases;
+        Thread sendingThread = new Thread(sendingTask);
+        sendingThread.start();
+    }
+
+
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            if (update.getMessage().hasText()) {
+                if (update.getMessage().getText().equals("/start")) {
+                    ChatUser chatUser = new ChatUser();
+                    chatUser.setChatId(update.getMessage().getChatId());
+                    chatUser.setUsername(update.getMessage().getFrom().getUserName());
+                    userDAO.update(chatUser);
+                }
+            }
+        }
+    }
 
+    @SneakyThrows
+    private void sendReleases() {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        while (true) {
+            for (MetalareaRelease metalareaRelease : releaseDAO.findAll()) {
+                if (!metalareaRelease.isSent()) {
+                    for (ChatUser user : userDAO.findAll()) {
+                        sendMessage.setChatId(user.getChatId());
+                        sendMessage.setText(metalareaRelease.toString());
+                        execute(sendMessage);
+                        metalareaRelease.setSent(true);
+                        releaseDAO.update(metalareaRelease);
+                    }
+                    Thread.sleep(3_125);
+                }
+            }
+            Thread.sleep(300_000);
+        }
+    }
+
+    @SneakyThrows
+    private void updateReleases() {
+        MetalareaParser.INSTANCE.parseReleases().forEach(releaseDAO::update);
+        Thread.sleep(300_000);
     }
 
     @Override
--- src/main/java/by/pw/crr/metla/entities/MetalareaRelease.java
+++ src/main/java/by/pw/crr/metla/entities/MetalareaRelease.java
@@ -6,12 +6,14 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
 public class MetalareaRelease {
+
     @Id
     private Long id;
     private String bandName;
@@ -19,7 +21,7 @@ public class MetalareaRelease {
     private String albumYear;
     private String bandGenre;
     private String releaseLink;
-    private boolean isHot;
+    private boolean isSent;
 
     @Override
     public String toString() {
@@ -34,21 +36,16 @@ public class MetalareaRelease {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         MetalareaRelease that = (MetalareaRelease) o;
-
-        if (!bandName.equals(that.bandName)) return false;
-        if (!albumTitle.equals(that.albumTitle)) return false;
-        if (!albumYear.equals(that.albumYear)) return false;
-        return bandGenre.equals(that.bandGenre);
+        return id.equals(that.id)
+                && bandName.equals(that.bandName)
+                && albumTitle.equals(that.albumTitle)
+                && albumYear.equals(that.albumYear)
+                && bandGenre.equals(that.bandGenre);
     }
 
     @Override
     public int hashCode() {
-        int result = bandName.hashCode();
-        result = 42 * result + albumTitle.hashCode();
-        result = 42 * result + albumYear.hashCode();
-        result = 42 * result + bandGenre.hashCode();
-        return result;
+        return Objects.hash(id, bandName, albumTitle, albumYear, bandGenre);
     }
 }
